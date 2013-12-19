0b9de784a06263646aaee48821467cc0c36ef770
Eugene Kirdzei <evk@onliner.by>
2013-12-19 12:14:08 +0300

    Initial commit

---
+++ helper.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+# Created: 2013-12-18
+# Author: Eugene Kirdzei
+# Description: Universal script for updates
+
+# Define some colors first:
+red='\033[0;31m'
+RED='\033[1;31m'
+blue='\033[0;34m'
+BLUE='\033[1;34m'
+cayan='\033[0;36m'
+CAYAN='\033[1;36m'
+green='\033[0;32m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+# --> Nice. 
+
+if [ -f config.cfg ]; then
+    . config.cfg
+  else
+  if [ -r helper/config.cfg ]; then
+    . helper/config.cfg
+  else
+    echo -e "${RED}Конфигурационный файл не найден!${NC}"
+    exit 0
+  fi
+fi
+
+check_folder config
+
+clear
+
+function _exit() {
+clear
+echo -e "${RED}Hasta la vista, baby${NC}"
+}
+trap _exit 0
+
+echo "Onliner deployement script"
+
+function operations() {
+#clear
+echo
+echo -e "${RED}Please choose an action?
+------------------------------
+${GREEN}1. Deploy
+${RED}=========
+${GREEN}
+96. Change permissions
+97. Check disc space
+98. Clear screen
+99. Exit
+${RED}------------------------------
+${BLUE}Press Ctrl+C or type 'exit' for exit${NC}"
+
+read -p '=> ' op
+
+KEY=$(date +%y-%m-%d_%H-%M)
+
+case $op in
+    "1" )
+        echo "What project to deploy?"
+        read -r -p '<l> for list=> ' action
+
+        case $action in
+            "l" )
+            projects_list
+            read -p '=> ' action
+            ;;&
+            * )
+              if [ -r $script_path/config/$action.cfg ]; then
+                #Load config
+                . $script_path/config/$action.cfg
+
+                read -r -p "Which branch? [master] (<l> for list) => " branch
+
+                case $branch in
+                    "l" )
+                        cd $project_path
+                        git ls-remote --heads origin  | sed 's?.*refs/heads/??'
+                        read -p '=> ' branch
+                    ;;&
+                    * )
+                        if [ "$branch" == '' ]; then
+                            branch='master'
+                        fi
+
+                        cd $project_path && git fetch -p && git checkout -f $branch
+
+                        read -r -p "Update composer? [y/N] => " response
+                        response=${response,,}
+                        if [[ $response =~ ^(yes|y)$ ]]; then
+                            if hash composer 2> /dev/null; then
+                              composer --no-dev update
+                            else
+                              php composer.phar --no-dev update
+                            fi
+                        fi
+                    ;;
+                esac
+              else
+                    #pwd
+                  echo -e "${RED}Конфигурационный файл для проекта $action не найден!${NC}"
+              fi
+            ;;
+        esac
+        cd $script_path
+    ;;
+    "96" )
+        echo "Change premissions"
+        cd $project_path
+        sudo chmod -R g+rw ./
+        echo
+    ;;
+    "97" )
+        df -h
+    ;;
+    "98" )
+        clear
+    ;;
+    "99"|exit )
+        exit 0
+    ;;
+esac
+operations
+}
+
+check_folder() {
+    if [ -z "$1" ]
+        then
+        echo "Folder name is not specified"
+        return 0
+    fi
+    
+    if [ -d "$1" ]
+        then
+            echo -n "The folder $1 exists"
+            echo
+        else
+            echo -n "Trying to create a folder $1"
+            echo
+            mkdir $1
+            check_folder $1
+    fi
+
+    return 0
+}
+
+projects_list() {
+  cd config/;
+  echo -e "${BLUE}Available projects:${GREEN}"
+  for i in *.cfg; do
+    echo ${i%\.*};
+  done
+  echo -e ${NC}
+}
+
+operations
+echo
+exit 0
+
